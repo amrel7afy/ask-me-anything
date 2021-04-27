5d6c2fb7cd4490be6b4f415a715272fa77318a8e
amrel7afy <amr.el7afy@gmail.com>
2021-04-27 17:11:21 +0200

    inital

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main (){
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int question=2;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[800],
+        title: Text('Ask Me Anything',style: TextStyle(color: Colors.white,fontSize: 30),),
       ),
+      backgroundColor: Colors.blue,
+      body: Center(child: FlatButton(
+        onPressed: (){setState(() {
+          question=Random().nextInt(5)+1;
+        });},
+          child: Image(image: AssetImage('images/ball$question.png'),))),
     );
+  }
+}
